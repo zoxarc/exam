b734f72a726259522158a273a20049078b581161
Nerya <thatneryamadar@gmail.com>
2022-11-04 18:22:45 +0200

    updated general_server.py

--- objects/template/general_server.py
+++ objects/template/general_server.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 from socket import create_server, socket
+from pickle import dumps, loads
 
 from protocol import Protocol
 from objects.template import Event, Handle
@@ -12,13 +14,28 @@ class ClientHandle:
     def __str__(self):
         return ':'.join(self.address)
 
+    def receive(self):
+        size = self.connection.recv(Protocol.SIZE_BUFFER)
+        size = int.from_bytes(size, Protocol.BYTE_ORDER)
+        stream = self.connection.recv(size)
+        return loads(stream)
+
+    def send(self, data, sender: GeneralServer):
+        sender.server_message(data, sender=self, handled=Handle())
+
+        stream = dumps(data)
+        size = len(stream).to_bytes(Protocol.SIZE_BUFFER, Protocol.BYTE_ORDER)
+        self.connection.send(size + stream)
+
 
 class GeneralServer:
     def __init__(self):
         self.server: socket = create_server(Protocol.ADDRESS)
         self.server.settimeout(Protocol.SERVER_TIMEOUT)
+        self.server.listen()
 
         self.clients: set[ClientHandle] = set()
+        self.running = True
 
         # events
         self.client_message: Event = Event()
@@ -45,11 +62,19 @@ class GeneralServer:
 
         return True
 
-    def receive(self):
-        pass
+    def receive_all(self):
+        for client in self.clients:
+            try:
+                data = client.receive()
 
-    def send(self):
-        pass
+            except TimeoutError:
+                continue
+
+            self.client_message(data, handled=Handle(), sender=self)
 
     def main_loop(self):
-        pass
+        while self.running:
+            self.accept()
+            self.receive_all()
+
+        self.closing(sender=self, handled=Handle())
--- protocol.py
+++ protocol.py
@@ -2,3 +2,5 @@ class Protocol:
     ADDRESS = ("127.0.0.1", 8080)
     SERVER_TIMEOUT = 0.1
     CLIENT_TIMEOUT = 0.1
+    SIZE_BUFFER = 4
+    BYTE_ORDER = "little"
