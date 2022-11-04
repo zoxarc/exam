a712a9c98c41bea8393993ec781b5ea44451cd30
Nerya <thatneryamadar@gmail.com>
2022-11-04 11:37:24 +0200

    reogranizing objects

---
+++ objects/__init__.py
@@ -0,0 +1,9 @@
+from letter import Letter
+from dletter import DLetter
+from paper import Paper
+
+__all__ = [
+    'Letter',
+    'DLetter',
+    'Paper'
+]
---
+++ objects/dletter.py
@@ -0,0 +1,2 @@
+class DLetter:
+    pass
