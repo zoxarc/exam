3af4661522ac321cd0ab4b4534d74f1022cfcdc3
Nerya <thatneryamadar@gmail.com>
2022-11-05 11:33:59 +0200

    name changes*

--- objects/double_envelope.py
+++ objects/double_envelope.py
@@ -2,7 +2,7 @@ from objects import Envelope
 
 
 class DoubleEnvelope:
-    def __init__(self, letter: Envelope, name: str, id_: int):
-        self.letter = letter
+    def __init__(self, envelope: Envelope, name: str, id_: int):
+        self.envelope = envelope
         self.name = name
         self.id = id_
