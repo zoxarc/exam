872bee953f3d0ae516aac3eb829bef26a2470969
cyber <roeicohensidon@gmail.com>
2022-11-07 16:35:08 +0200

    discription

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 from objects import *
+from protocol import Protocol as P
 
 
-def dcount(vlist: dict, env: list[DoubleEnvelope]):
+def dcount(vlist: dict, env: list[DoubleEnvelope]) -> dict:
     global votes
-    dvoters = [a for a in env if not a.id in list(vlist.keys())]
+    dvoters = [a for a in env if a.id not in list(vlist.keys())]
 
     for a in dvoters:
          votes.update({a.envelope.notes[0], votes.get(a.envelope.notes[0]) + 1})
+    return votes
 
-def ncount(vote: envelope):
-    global votes
-    pass
 
+#def ncount()
 
 
 def main():
@@ -20,3 +20,4 @@ def main():
 if __name__ == "__main__":
     votes = {party: 0 for party in P.PARTIES}
     main()
+
