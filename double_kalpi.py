d6e155979b7103f12c6f0c201eec146343d4f3a0
Nerya <thatneryamadar@gmail.com>
2022-11-07 16:46:16 +0200

    deleted yuvalido

---
+++ double_kalpi.py
@@ -0,0 +1,29 @@
+from template import GeneralClient
+from objects import Envelope, DoubleEnvelope, Note
+from protocol import Protocol
+from clear_screen import clear
+
+
+class DoubleKalpi:
+    def __init__(self):
+        self.client = GeneralClient()
+
+    def vote(self):
+        party = input(f"choose a party:\n{', '.join(Protocol.PARTIES)} ")
+
+        if Protocol.IsParty(party):
+            note = Note(party)
+            envelope = Envelope([note])
+            double_envelope = DoubleEnvelope(envelope, input("enter name: "), int(input("enter id: ")))
+
+            self.client.send(double_envelope)
+
+    def run(self):
+        while True:
+            self.vote()
+            clear()
+
+
+if __name__ == '__main__':
+    kalpi = DoubleKalpi()
+    kalpi.run()
