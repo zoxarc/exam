a4c31870a3b76e4d43833091670222880ce89a1e
zoxarc <zoxarc@gmail.com>
2022-11-07 16:28:48 +0200

    started normal votes

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 from objects import *
 
 
-def dcount(vlist: dict, env: list[DoubleEnvelope]) -> dict:
+def dcount(vlist: dict, env: list[DoubleEnvelope]):
     global votes
     dvoters = [for a in env if not a.id in list(vlist.keys())]
 
     for a in dvoters:
          votes.update({a.envelope.notes[0], votes.get(a.envelope.notes[0]) + 1})
-    return votes
 
-#def ncount()
+def ncount(vote: envelope):
+    global votes
+    pass
+
 
 
 def main():
     pass
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     votes = {party: 0 for party in P.PARTIES}
     main()
