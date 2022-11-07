5c9ba29bc58c1f34d3d3ac60c86f62aadadf76db
zoxarc <zoxarc@gmail.com>
2022-11-07 16:51:00 +0200

    almost finished server

--- server.py
+++ server.py
@@ -1,23 +1,40 @@
 from objects import *
 from protocol import Protocol as P
+from sock_lib import GeneralServer
+s = GeneralServer()
 
 
-def dcount(vlist: dict, env: list[DoubleEnvelope]) -> dict:
+def dcount(env: list[DoubleEnvelope]) -> dict:
     global votes
     dvoters = [a for a in env if a.id not in list(vlist.keys())]
 
     for a in dvoters:
          votes.update({a.envelope.notes[0], votes.get(a.envelope.notes[0]) + 1})
-    return votes
 
 
-#def ncount()
+def ncount(env: Envelope):
+    global votes
+    votes.update({env.notes[0], votes.get(env.notes[0]) + 1})
+
+
+@s.event("client_message")
+async def on_client_message(sender, message):
+    match type(message):
+        case Envelope:
+            if message.id not in vdict:
+                vdict.update(message.id: message.name)
+        case DoubleEnvelope:
+            ddict.update(message)
+
+
 
 
 def main():
-    pass
+    s.main_loop()
 
 if __name__ == "__main__":
     votes = {party: 0 for party in P.PARTIES}
+    vdict = {}
+    ddict = {}
     main()
 
