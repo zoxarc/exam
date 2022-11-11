b46743e12cc2f3a734267c825e25fd449bb41296
Nerya <thatneryamadar@gmail.com>
2022-11-11 10:41:41 +0200

    no pop in envelope

--- objects/envelope.py
+++ objects/envelope.py
@@ -8,7 +8,7 @@ class Envelope:
         self.notes: list[Note] = notes
 
     def status(self) -> tuple[bool, Optional[str]]:
-        parties = set(note.party for note in self.notes)
+        parties = list(note.party for note in self.notes)
         valid = 0 < len(self.notes) < 6 and len(parties) == 1
 
-        return valid, parties.pop() if valid else None
+        return valid, parties[0] if valid else None
